ece43b4889f3b8302acb94ecf4ab9dd5c93d9544
seojw0730 <seojw0730@naver.com>
2024-04-16 19:51:50 +0900

    최신 교육 제목 출력 및 교육 등록 기능 추가

--- src/main/java/education/controller/EduBookFormController.java
+++ src/main/java/education/controller/EduBookFormController.java
@@ -7,12 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import education.model.dto.EduRecentDto;
+import education.model.service.EduService;
+
 /**
  * Servlet implementation class EduBookFormController
  */
 @WebServlet("/edu/book/form")
 public class EduBookFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private EduService service = new EduService();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -25,6 +29,11 @@ public class EduBookFormController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EduRecentDto dto = service.selectRecent();
+		Object recentEdu = (dto == null) ? "등록된 교육이 없습니다" : dto.getEduSubject();
+		request.setAttribute("recentEdu", recentEdu);
+		
+		
 		request.getRequestDispatcher("/WEB-INF/views/edu/edubookform.jsp").forward(request, response);
 	}
 

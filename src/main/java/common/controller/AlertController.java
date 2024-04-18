fadceea7db59600c7da533d90d01ab1e2de3ae39
seojw0730 <seojw0730@naver.com>
2024-04-18 16:32:18 +0900

    Update AlertController.java

--- src/main/java/common/controller/AlertController.java
+++ src/main/java/common/controller/AlertController.java
@@ -9,10 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import member.model.dto.MemberInfoDto;
 
 public class AlertController {
-	public static void loginPermission(HttpServletRequest request, HttpServletResponse response, String msg) throws ServletException, IOException {
+	public static void loginPermission(HttpServletRequest request, HttpServletResponse response, String msg, String jspPath) throws ServletException, IOException {
 		if(request.getSession().getAttribute("ssslogin") == null) {
 			request.setAttribute("alertMsg", msg);
-			request.getRequestDispatcher("/WEB-INF/views/edu/eduone.jsp").forward(request, response);
 			return;
 		}
 	}

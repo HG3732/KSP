bf62c0766fa496392b2bc334107dce717073ad92
seojw0730 <seojw0730@naver.com>
2024-04-18 15:34:44 +0900

    로그인 및 관리자 경고창 함수

---
+++ src/main/java/common/controller/AlertController.java
@@ -0,0 +1,28 @@
+package common.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.model.dto.MemberInfoDto;
+
+public class AlertController {
+	public static void loginPermission(HttpServletRequest request, HttpServletResponse response, String msg) throws ServletException, IOException {
+		if(request.getSession().getAttribute("ssslogin") == null) {
+			request.setAttribute("alertMsg", msg);
+			request.getRequestDispatcher("/WEB-INF/views/edu/eduone.jsp").forward(request, response);
+			return;
+		}
+	}
+	public static void adminPermission(HttpServletRequest request, HttpServletResponse response, String msg, String jspPath, String servletMapping) throws ServletException, IOException {
+		MemberInfoDto dto = (MemberInfoDto) request.getSession().getAttribute("ssslogin");
+		if(dto.getMem_admin() < 1 || dto == null) {
+			request.setAttribute("alertMsg", msg);
+			request.setAttribute("servletMapping", servletMapping);
+			request.getRequestDispatcher(jspPath).forward(request, response);
+			return;
+		}
+	}
+}

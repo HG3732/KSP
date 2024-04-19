63e6afa9c350f07369ee4484905a8de82df77852
seojw0730 <seojw0730@naver.com>
2024-04-19 18:40:58 +0900

    Update Gmail.java

--- src/main/java/education/book/controller/Gmail.java
+++ src/main/java/education/book/controller/Gmail.java
@@ -6,6 +6,7 @@ import javax.mail.PasswordAuthentication;
 public class Gmail extends Authenticator {
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("seojw0730@gmail.com", "bwrw bzec mqet ajct");
+//		return new PasswordAuthentication("seojw0730@gmail.com", "bwrw bzec mqet ajct");
+		return new PasswordAuthentication("구글 이메일", "앱 비밀번호");
 	}
 }

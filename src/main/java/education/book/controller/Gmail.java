181a8d889c1b5043d19cb6e0b09110b7fd8e7a0a
seojw0730 <seojw0730@naver.com>
2024-04-20 19:40:48 +0900

    이메일 발송 계정설정

--- src/main/java/education/book/controller/Gmail.java
+++ src/main/java/education/book/controller/Gmail.java
@@ -6,7 +6,7 @@ import javax.mail.PasswordAuthentication;
 public class Gmail extends Authenticator {
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-//		return new PasswordAuthentication("seojw0730@gmail.com", "bwrw bzec mqet ajct");
-		return new PasswordAuthentication("구글 이메일", "앱 비밀번호");
+		return new PasswordAuthentication("seojw0730@gmail.com", "bwrw bzec mqet ajct");
+//		return new PasswordAuthentication("구글 이메일", "앱 비밀번호");
 	}
 }

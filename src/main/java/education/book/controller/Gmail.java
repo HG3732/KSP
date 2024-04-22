2564819c1af3eea977366a1ff0b6ad124fcc88aa
seojw0730 <seojw0730@naver.com>
2024-04-22 11:17:38 +0900

    구글 비밀번호 숨기기

--- src/main/java/education/book/controller/Gmail.java
+++ src/main/java/education/book/controller/Gmail.java
@@ -1,12 +1,22 @@
 package education.book.controller;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 
 public class Gmail extends Authenticator {
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("seojw0730@gmail.com", "bwrw bzec mqet ajct");
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileReader("/KSP/resources/driver.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new PasswordAuthentication(prop.getProperty("gmail.email"), prop.getProperty("gmail.password"));
 //		return new PasswordAuthentication("구글 이메일", "앱 비밀번호");
 	}
 }

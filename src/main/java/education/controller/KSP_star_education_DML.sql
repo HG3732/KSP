b6ac6b733c95b4c262063a5d53e2b9af95f33ca3
seojw0730 <seojw0730@naver.com>
2024-05-02 11:11:52 +0900

    재원) ~ DML파일에 트리거 등록

--- src/main/java/education/controller/KSP_star_education_DML.sql
+++ src/main/java/education/controller/KSP_star_education_DML.sql
@@ -1,5 +1,32 @@
+-- 교육 신청시 정원 초과 여부 확인 후 신청 내역 등록
+create or replace trigger trg_edu_book_num
+    before insert on edu_book
+    referencing new as new 
+    for each row
+declare
+    vb number;
+begin
+    select (edu_max_num-edu_book_num) into vb from edu_list where edu_id = :new.edu_id;
+    if vb >= :new.edu_part_num then
+        update edu_list set edu_book_num = edu_book_num + :new.edu_part_num where edu_id = :new.edu_id and edu_max_num >= edu_book_num + :new.edu_part_num;
+    else
+        raise_application_error(-20301, '정원 초과로 신청 불가');
+    end if;
+end;
+/
+-- 교육 취소시 교육 신청 인원 감소
+create or replace trigger trg_edu_book_cancel
+    before delete on edu_book
+    referencing new as new 
+    for each row
+begin
+    update edu_list set edu_book_num = edu_book_num - :new.edu_part_num where edu_id = :new.edu_id;
+end;
+/
+
 SELECT * FROM EDU_LIST ORDER BY EDU_ID;
 DESC EDU_LIST;
+-- 임의의 교육 등록
 BEGIN
     FOR I IN 1..123 LOOP
         INSERT INTO EDU_LIST VALUES(
